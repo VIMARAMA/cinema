65d039b1cb830b074ab8d26f74bd1ad1bae4a6c8
MarianaBMoraes <marianabmoraes.ef@gmail.com>
2024-08-20 20:37:05 -0300

    feat: add CRUD 'sessão.js'

--- src/controllers/sessao.js
+++ src/controllers/sessao.js
@@ -4,23 +4,31 @@ const sala_controller = require("./sala.js");
 const db = [];
 let nextId = 1;
 
+const validarCadeiras = (cadeira) => {
+  const cadeirasValidas = Array.from({ length: 10 }, (_, i) => `${i + 1}a`);
+  return cadeirasValidas.includes(cadeira);
+};
+
 const model = (sessao, id = nextId++) => {
+  
   if (
     sessao.data > 0 &&
     sessao.hora > 0 &&
     sessao.filme_id != undefined &&
-    sessao.sala_id !=
-      undefined + //FIXME
-        filme_controller.show(sessao.filme_id) &&
-    sala_controller.show(sessao.sala_id) //FIXME
-  )
+    sessao.sala_id != undefined &&
+    filme_controller.show(sessao.filme_id) &&
+    sala_controller.show(sessao.sala_id) &&
+    sessao.cadeiras && validarCadeiras(sessao.cadeiras)
+  ) {
     return {
       id,
       data: sessao.data,
       hora: sessao.hora,
       filme_id: sessao.filme_id,
       sala_id: sessao.sala_id,
+      cadeiras: sessao.cadeiras
     };
+  }
 };
 
 const store = (body) => {
@@ -44,7 +52,6 @@ const update = (id, body) => {
 
   if (novo && index != -1) {
     db[index] = novo;
-
     return 200;
   }
 
